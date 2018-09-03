b9785a8ce2d5f7f8e9240f2bd7bb9d497feea596
Maxime Barber <maximebarber@Maximes-MacBook-Air.local>
2018-09-03 15:31:53 +0200

    Initial commit

---
+++ resources/views/pages/map.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+
+        <br><h1>Map interactive</h1><br>
+
+        <form action="" id="departement">
+
+                <!-- Numéro de département -->
+                <div class="form-group row">
+                    
+                    <label for="departement" class="col-sm-12 col-form-label">J'entre mon département (uniquement 33, 34, 67, 68)</label>
+
+                </div>
+                <div class="form-group row">
+
+                    <div class="col-sm-3">
+
+                        <input style="border: 1px solid black" type="number" class="form-control-plaintext" id="departementNumber">
+
+                    </div>
+
+                    <button type="button" class="btn btn-outline-primary" onclick="getDepartements()">Valider</button>
+
+                    
+                </div>
+
+                <!-- Géolocalisation -->
+                <div class="form-group row">
+                    
+                    <label for="departement" class="col-sm-12 col-form-label">Je me géo-localise</label>
+
+                </div>
+                <div class="form-group row">
+
+                    <button style="margin-left: 15px;" type="button" class="btn btn-outline-primary" onclick="getLocation()">Me localiser</button>
+                    
+                </div>
+
+        </form><br>
+
+    </div>
+
+    <div id="mapid"></div>
+
+    <br><div id="msg"></div>
+
+</div>
+
+@endsection
